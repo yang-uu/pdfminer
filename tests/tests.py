d8e4a16ad4ca418b5c03afaa39a6ea3e51851c8f
Nils <hermansson.nils@gmail.com>
2022-09-06 14:11:58 +0200

    Added another test

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ import unittest
 from pdfminer.runlength import rldecode
 from pdfminer.psparser import KWD, LIT, PSEOF, PSBaseParser, PSStackParser
 from pdfminer.ccitt import CCITTG4Parser
-from ..pdfminer.utils import url
+from pdfminer.utils import url
 
 
 
@@ -275,7 +275,11 @@ class TestUtilities(unittest.TestCase):
 
     def test_url_special_characters(self):
         test_url = url("testsite.com/search?", name="Äööl_&==??!!  ")
-        self.assertEqual(True, False)
+        self.assertEqual(test_url, "testsite.com/search?name=%C3%84%C3%B6%C3%B6l_%26%3D%3D%3F%3F%21%21++")
+    
+    def test_url_empty(self):
+        test_url = url("testsite.com/search?", name="")
+        self.assertEqual(test_url, "testsite.com/search?name=")
 
 if __name__ == '__main__':
     unittest.main()
