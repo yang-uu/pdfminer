296396f54e749a5d8870060050938aa75c607291
aronf <aronfreyrh@gmail.com>
2022-09-08 14:18:49 +0200

    Changes by Chril

--- tests/test_pdf2txt.py
+++ tests/test_pdf2txt.py
@@ -10,12 +10,12 @@ class TestPdf2Text(unittest.TestCase):
 
     def test_convert_from_pdf_text(self):
         expected_output = ""
-        expected_filename = 'test_files/test_convert_from_pdf_text_expected.txt'
+        expected_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_from_pdf_text_expected.txt')
         with open(expected_filename, 'r') as file:
             expected_output = file.read()
 
-        test_filename = 'test_files/test_convert_from_pdf_text_result.txt'
-        filename = ['../samples/simple1.pdf']
+        test_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_from_pdf_text_result.txt')
+        filename = [os.path.join(os.path.dirname(__file__), '../samples/simple1.pdf')]
         convert_from_pdf(filename, ConverterParams(pagenos=set(), laparams=LAParams()), OutputType.TEXT, test_filename)
 
         # assert that the output file contains the expected output
@@ -24,13 +24,13 @@ class TestPdf2Text(unittest.TestCase):
             self.assertEqual(contents, expected_output)
 
     def test_convert_simple1_pdf_file_to_text(self):
-        expected_filename = os.path.join(PATH, 'test_files/test_convert_simple1_pdf_file_to_text_expected.txt')
+        expected_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_text_expected.txt')
         with open(expected_filename, 'r') as file:
             expected_output = file.read()
 
-        test_filename = 'test_files/test_convert_simple1_pdf_file_to_text_result.txt'
+        test_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_text_result.txt')
         options = [('-o', test_filename), ('-t', 'text')]
-        filename = ['../samples/simple1.pdf']
+        filename = [os.path.join(os.path.dirname(__file__), '../samples/simple1.pdf')]
         handle_input_variables(options, filename)
 
         # assert that the output file contains the expected output
@@ -39,13 +39,13 @@ class TestPdf2Text(unittest.TestCase):
             self.assertEqual(contents, expected_output)
 
     def test_convert_simple1_pdf_file_to_xml(self):
-        expected_filename = 'test_files/test_convert_simple1_pdf_file_to_xml_expected.xml'
+        expected_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_xml_expected.xml')
         with open(expected_filename, 'r') as file:
             expected_output = file.read()
 
-        test_filename = 'test_files/test_convert_simple1_pdf_file_to_xml_result.xml'
+        test_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_xml_result.xml')
         options = [('-o', test_filename), ('-t', 'xml')]
-        filename = ['../samples/simple1.pdf']
+        filename = [os.path.join(os.path.dirname(__file__), '../samples/simple1.pdf')]
         handle_input_variables(options, filename)
 
         # assert that the output file contains the expected output
@@ -54,13 +54,13 @@ class TestPdf2Text(unittest.TestCase):
             self.assertEqual(contents, expected_output)
 
     def test_convert_simple1_pdf_file_to_html(self):
-        expected_filename = 'test_files/test_convert_simple1_pdf_file_to_html_expected.html'
+        expected_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_html_expected.html')
         with open(expected_filename, 'r') as file:
             expected_output = file.read()
 
-        test_filename = 'test_files/test_convert_simple1_pdf_file_to_html_result.html'
+        test_filename = os.path.join(os.path.dirname(__file__), 'test_files/test_convert_simple1_pdf_file_to_html_result.html')
         options = [('-o', test_filename), ('-t', 'html')]
-        filename = ['../samples/simple1.pdf']
+        filename = [os.path.join(os.path.dirname(__file__), '../samples/simple1.pdf')]
         handle_input_variables(options, filename)
 
         # assert that the output file contains the expected output
