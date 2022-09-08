a5044466481683ae9e1701c80c35bca5e6a0491b
Oier Mentxaka <oiermentxaka@gmail.com>
2022-09-08 18:31:17 +0200

    Update test_lzw.py

--- tests/test_lzw.py
+++ tests/test_lzw.py
@@ -8,7 +8,8 @@ class TestLzwDecode(unittest.TestCase):
 
     # lzwdecode
     def test_lzwdecode(self):
-        self.assertEqual(lzwdecode(bytes.fromhex('800b6050220c0c8501')), b'-----A---B')
+        self.assertEqual(lzwdecode(
+            bytes.fromhex('800b6050220c0c8501')), b'-----A---B')
 
 
 if __name__ == '__main__':
