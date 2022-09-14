522f8745dc334b84fd9b024d61b65081cebc2bb1
PrimeNumb <12763608+PrimeNumb@users.noreply.github.com>
2022-09-14 17:10:17 +0200

    Added test files for pdffont feature

---
+++ tests/test_pdffont.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestPDFFont(unittest.TestCase):
+
+    def test_arial(self):
+        self.assertFalse(True)
+
+    def test_timesnewroman_arial(self):
+        self.assertFalse(True)
+
+    def test_timesnewroman_arial_verdana(self):
+        self.assertFalse(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
